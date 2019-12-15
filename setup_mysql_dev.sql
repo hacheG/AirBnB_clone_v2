9722dbac14367cdb5da7ce0d2cf2e4c09a776eb9
hacheG <943@holbertonschool.com>
2019-12-15 02:03:35 +0000

    [FEATURE] modify gitignore & add sql_dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,1 +1,6 @@
-# fucking holberton
+-- beauty comment for the fucking holberton
+CREATE DATA BASE IF NOT EXISTS hbnb_dev_db;
+CREATE IF NOT EXISTS USER 'hbnb_dev@localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
