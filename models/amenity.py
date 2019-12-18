d5d49c65fff2b55c64da3b807fb1a4a2d5b37592
hacheG <943@holbertonschool.com>
2019-12-18 01:21:06 -0500

    [FIX] remove gitLog conflict

--- models/amenity.py
+++ models/amenity.py
@@ -12,10 +12,6 @@ class Amenity(BaseModel, Base):
     """
     #    __tablename__ = 'amenities'
 
-<<<<<<< HEAD
     name = Column(String(128), nullalble=False))
     place_amenities = relationship("Place", backref="amenities")
-=======
-    #   name = Column(String(128), nullalble=False)
-    name = ""
->>>>>>> 7039353de6cd3e6b6d1ab7bd04454efeed567a2d
+
