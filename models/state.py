08af89cdf4c24d1958daba052a73a7e763b1feec
hacheG <943@holbertonschool.com>
2019-12-18 18:07:15 -0500

    [FIX] cambie el value por el items

--- models/state.py
+++ models/state.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from models.city import City
-import models
+from models.storage import Storage
 from os import getenv
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship, backref
@@ -13,21 +13,19 @@ class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
+    """
     if getenv("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
-        cities = relationship("City", cascade="all, delete", backref="state")
+        cities = relationship("City", backref=backref("state", cascade="all, delete-orphan")
     else:
         name = ""
 
     @property
     def cities(self):
-        Cities
+        """Cities"""
         x = []
-        for value in models.storage.all(City).values():
+        for key, value in storage.all(City).items():
             if value.state_id == self.id:
                 x.append(value)
         return x
-    """
-    name = ""
-#  cities = relationship("City", cascade="all, delete-orphan", backref="state")
