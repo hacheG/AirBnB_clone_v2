06914fa8281dc5f9564b73bbb76daa6e1e9a1d4b
hacheG <943@holbertonschool.com>
2019-12-18 13:37:33 -0500

    [FIX] classes & style pep8

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -8,9 +8,11 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, scoped_session
 from os import getenv
+from sqlalchemy.ext.declarative import declarative_base
+
 
 class DBStorage:
     """This class serializes instances to a JSON file and
@@ -37,14 +39,16 @@ class DBStorage:
         """
         Returns a dictionary
         """
-        my_objects = dict()
+        my_objects = []
         objects = ["State", "City", "User", "Place", "Review", "Amenity"]
 
-        for objts in objects:
-            for obj in self.__session.query(eval(objts)).all():
-                my_objects[type(obj).__name__+"."+obj.id] = obj
-
-        return(my_objects)
+        if cls:
+            my_objects = self.__session.query(cls)
+        else:
+            for cls in objects:
+                my_objects = my_objects + self.__session.query(cls)
+        return {type(value).__name__ + "." + value.id: value
+                for value in my_objects}
 
     def new(self, obj):
         """sets __object to given obj
@@ -62,8 +66,9 @@ class DBStorage:
         """serialize the file path to JSON file path
         """
         Base.metadata.create_all(self.__engine)
-        self.__session = scoped_session(sessionmaker(expire_on_commit=False,
-                                        bind=self.__engine))()
+        newSession = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(newSession)
+        self.session = Session()
 
     def delete(self, obj=None):
         """
