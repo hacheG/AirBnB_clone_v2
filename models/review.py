49b6b71a7e74160e5c3ca054f9daf8fc405f1ae1
hacheG <943@holbertonschool.com>
2019-12-18 06:28:35 -0500

    [FIX] review & updated

--- models/review.py
+++ models/review.py
@@ -10,10 +10,9 @@ class Review(BaseModel, Base):
         place_id: place id
         user_id: user id
         text: review description
-
+    """
     __tablename__ = "reviews"
 
     place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     text = Column(String(1024), nullable=False)
-    """
