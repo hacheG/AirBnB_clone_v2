b26f688b7d8ed575660fafc9ae1abfd81bf6db1a
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2019-12-19 20:38:26 +0000

    [Fix]

--- models/amenity.py
+++ models/amenity.py
@@ -2,6 +2,7 @@
 """This is the amenity class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy import relationship
 
 
 class Amenity(BaseModel, Base):
@@ -11,5 +12,5 @@ class Amenity(BaseModel, Base):
     """
     #    __tablename__ = 'amenities'
 
-    #   name = Column(String(128), nullalble=False)
-    name = ""
+    name = Column(String(128), nullalble=False))
+    place_amenities = relationship("Place", backref="amenities")
--- models/review.py
+++ models/review.py
@@ -11,9 +11,9 @@ class Review(BaseModel, Base):
         user_id: user id
         text: review description
 
-    __tablename__ = 'reviews'
+    __tablename__ = "reviews"
 
-    place_id = Column(String(60), nullable=False, ForeignKey('places.id'))
-    user_id = Column(String(60), nullable=False, ForeignKey('users.id'))
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     text = Column(String(1024), nullable=False)
     """
