e33f98bcb9b864477c6bd802b0420e329a234ff8
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2019-12-21 00:46:07 +0000

    [Fix]back to original

--- models/place.py
+++ models/place.py
@@ -35,14 +35,3 @@ class Place(BaseModel):
     reviewInstances.append(value)
     return (reviewInstances)
     """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
