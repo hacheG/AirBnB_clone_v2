7e5563ab4e64fad8fcd99a19a7c2967d4919cfac
hacheG <943@holbertonschool.com>
2020-01-03 09:36:34 -0500

    [FEATURE] add task2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,1 +1,34 @@
-#sfghsdfhs
+#!/usr/bin/python3
+# bla bla bla bla
+
+from fabric.api import *
+import os
+
+env.hosts = ["35.237.254.224", "34.73.109.66"]
+env.user = "ubuntu"
+
+
+def do_deploy(archive_path):
+    """ line of comment"""
+    if not os.path.exists(archive_path):
+        return False
+
+    results = []
+
+    res = put(archive_path, "/tmp")
+    results.append(res.succeeded)
+
+    basename = os.path.basename(archive_path)
+    if basename[-4:] == ".tgz":
+        name = basename[:-4]
+    newdir = "/data/web_static/releases/" + name
+    run("mkdir -p " + newdir)
+    run("tar -xzf /tmp/" + basename + " -C " + newdir)
+
+    run("rm /tmp/" + basename)
+    run("mv " + newdir + "/web_static/* " + newdir)
+    run("rm -rf " + newdir + "/web_static")
+    run("rm -rf /data/web_static/current")
+    run("ln -s " + newdir + " /data/web_static/current")
+
+    return True
