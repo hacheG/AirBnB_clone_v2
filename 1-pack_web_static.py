df27297bbe83885c2d3ac2b44cbf4f761e1cf802
hacheG <943@holbertonschool.com>
2020-01-03 09:02:04 -0500

    [FEATURE] the task boring

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,1 +1,20 @@
-#asdfasdfa
+#!/usr/bin/python3
+# naaaaa paila
+
+import tarfile
+import os
+from datetime import datetime
+
+
+def do_pack():
+    """ function do_pack """
+    savedir = "versions/"
+    filename = "web_static_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".tgz"
+    if not os.path.exists(savedir):
+        os.mkdir(savedir)
+    with tarfile.open(savedir + filename, "w:gz") as tar:
+        tar.add("web_static", arcname=os.path.basename("web_static"))
+    if os.path.exists(savedir + filename):
+        return savedir + filename
+    else:
+        return None
