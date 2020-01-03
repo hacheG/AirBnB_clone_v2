76cd8789ea3f833167c57d2995934809e6bc0340
hacheG <943@holbertonschool.com>
2020-01-03 04:55:22 -0500

    [FEATURE] add files

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#a bash script bla bla bla
+
+sudo mkdir /data/
+sudo mkdir /data/web_static/
+sudo mkdir /data/web_static/releases/
+sudo mkdir /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+echo "<html>
+	<head>
+	</head>
+	<body>
+		Holberton School
+	</body>
+	</html>" | sudo tee /data/web_static/releases/test/index.html
+
+sudo mkdir /data/web_static/shared/
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sed -i '/listen 80 default_server/a location /hbng_static {\n\t alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sudo service nginx restart
