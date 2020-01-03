c40a80c0ac55e9bfddb72c20ae956a0d89a78e4d
hacheG <943@holbertonschool.com>
2020-01-03 05:35:27 -0500

    [666_TRY] all changes + ufw

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -20,6 +20,6 @@ echo "<html>
 sudo mkdir /data/web_static/shared/
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
-sed -i '/listen 80 default_server/a location /hbng_static/ {\n\t alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server/a location /hbng_static/ {\n\t alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 sudo service nginx restart
 exit 0
