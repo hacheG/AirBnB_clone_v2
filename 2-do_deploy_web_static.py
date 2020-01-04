716878c2cf14deb4a18d1ad8c491d7a3e2d8ac06
hacheG <51682007+hacheG@users.noreply.github.com>
2020-01-04 00:04:18 -0500

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,6 +8,28 @@ env.hosts = ["35.237.238.105", "34.74.131.167"]
 env.user = "ubuntu"
 
 
+def deploy():
+    """ Calls all tasks to deploy archive to webservers"""
+    tar = do_pack()
+    if not tar:
+        return False
+    return do_deploy(tar)
+
+
+def do_pack():
+    """ Creates tar archive"""
+    savedir = "versions/"
+    filename = "web_static_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".tgz"
+    if not os.path.exists(savedir):
+        os.mkdir(savedir)
+    with tarfile.open(savedir + filename, "w:gz") as tar:
+        tar.add("web_static", arcname=os.path.basename("web_static"))
+    if os.path.exists(savedir + filename):
+        return savedir + filename
+    else:
+        return None
+
+
 def do_deploy(archive_path):
     """ Deploys archive to servers"""
     if not os.path.exists(archive_path):
