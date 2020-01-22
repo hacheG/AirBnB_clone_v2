e1d144245cfd3e8541204d6e9f595e54a198272b
hacheG <943@holbertonschool.com>
2020-01-22 16:44:31 -0500

    [FEATURE] add task_4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""a prgram with flask"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def fun():
+    """a program with flask"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def fun2():
+    """a program with flask"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def fun3(text):
+    """a program with flask"""
+    return "C %s" % text.replace("_", " ")
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def fun4(text="is cool"):
+    """a program with flask"""
+    return "Python %s" % text.replace("_", " ")
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def fun5(n):
+    """a program with flask"""
+    return "%d is a number" % n
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
+#    app.run()
