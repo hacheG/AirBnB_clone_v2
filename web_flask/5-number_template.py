106775dec81c78ec8b9f81464cc37a313f152bec
hacheG <943@holbertonschool.com>
2020-01-22 17:19:01 -0500

    [FIX] change localhost to host=0.0.0.0 with a port 5000

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -45,5 +45,5 @@ def fun6(n):
     return render_template("5-number.html")
 
 if __name__ == "__main__":
-#    app.run(host="0.0.0.0", port="5000")
-    app.run()
+    app.run(host="0.0.0.0", port="5000")
+#    app.run()
