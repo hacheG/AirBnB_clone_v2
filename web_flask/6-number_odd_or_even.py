b783933f13459087ae8814ed593e00ff4ffdf974
hacheG <943@holbertonschool.com>
2020-01-22 19:50:26 -0500

    [FIX] add new line at last

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -51,5 +51,5 @@ def fun7(n):
     return render_template("6-number_odd_or_even.html", n=n)
 
 if __name__ == "__main__":
-#    app.run(host="0.0.0.0", port="5000")
-    app.run()
+    app.run(host="0.0.0.0", port="5000")
+#    app.run()
