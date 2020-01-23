f5f89591d1054341afb0bd6e008d2acbf5fc729d
hacheG <943@holbertonschool.com>
2020-01-22 22:15:24 -0500

    [FEATURE] add task_7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""a prgram with flask"""
+
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def fun8():
+    """a program with flask"""
+    return storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def fun9():
+    """a program with flask"""
+    states = storage.all(State).values()
+    return render_template('7-states_list.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
+#    app.run()
